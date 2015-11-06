b5bc69b6a9ba9315ddac37593c41cb4e36b04fa9
Javier E. Perez P <javier.perez.puello@gmail.com>
2015-11-06 00:32:24 -0430

    It get the results expected

--- barter.pl
+++ barter.pl
@@ -1,8 +1,45 @@
 #!/usr/bin/perl
 
 @possible_numbers = qw/1 8 4 0 7 2/;
-@all_numbers = (@possible_numbers,qw/9 5 6 3/);
+# @all_numbers = (@possible_numbers,qw/9 5 6 3/);
 
-foreach (@all_numbers) {
-  print $_ . "\n";
+@first = @second = @third = @fourth = @possible_numbers ;
+%present = {};
+
+foreach $a (@first) {
+  $present{$a} = 1 ;
+
+  foreach $b (@second) {
+    if (!$present{$b}) {
+      $present{$b} = 1 ;
+      $solo_b{$b} = 1 ;
+
+      foreach $c (@third) {
+        if (!$present{$c}) {
+          $present{$c} = 1 ;
+          $solo_c{$c} = 1 ; # retiene variable
+
+          foreach $d (@fourth) {
+            if (!$present{$d}) {
+              $present{$d} = 1 ;
+
+              print $a . $b . $c . $d . "\n";
+
+              $present{$d} = 0 ;
+
+            }else{
+              next;
+            }
+          } # fin d
+          $present{$c} = 0 ; # libera la variable
+        }else{
+          next;
+        }
+      } # fin c
+      $present{$b} = 0 ;
+    }else{
+      next;
+    }
+  } # fin b
+  $present{$a} = 0 ;
 }
